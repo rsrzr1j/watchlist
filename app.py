33743e995adf17dfd30e013bfaf338c060f8cacf
ff <rsrzr1j@163.com>
2020-02-20 14:23:35 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask import url_for
 
 app=Flask(__name__)
 
+#准备虚拟数据
+name='yf zhang'
+movies=[
+	{'title': 'My Neighbor Totoro', 'year': '1988'},
+	{'title': 'Dead Poets Society', 'year': '1989'},
+	{'title': 'A Perfect World', 'year': '1993'},
+	{'title': 'Leon', 'year': '1994'},
+	{'title': 'Mahjong', 'year': '1996'},
+	{'title': 'Swallowtail Butterfly', 'year': '1996'}, 
+	{'title': 'King of Comedy', 'year': '1999'}, 
+	{'title': 'Devils on the Doorstep', 'year': '1999'},
+	{'title': 'WALL-E', 'year': '2008'},
+	{'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
-def hello():
-	return 'Hello'
-
-@app.route('/user/<name>')
-def user_page(name):
-	return '<h1>User :%s</h1>' % name
-
-@app.route('/test')
-def test_url_for():
-	#下面是一些调用示例
-	print(url_for('hello'))
-	print(url_for('user_page',name='gali'))
-	print(url_for('user_page',name='peter'))
-	# 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-	print(url_for('test_url_for',name=2))
-	return "Test page,please check them in your cmdline"
+def index():
+	return render_template('index.html',name=name,movies=movies)
